c814ff732883dc230bd903141e7d578c5afc20db
Rafael Beraldo <rafberaldo@gmail.com>
2021-11-29 18:25:09 -0300

    Update RequestUser.py

--- RequestUser.py
+++ RequestUser.py
@@ -4,9 +4,9 @@ querystring =
 {"fields":"id,username,locked,authType,role,locked,createdTime,group","":""}
 payload = ""
 headers = {
- 'cookie': "TNS_SESSIONID=01700a09687c64eb4eee08e4b76f47a8",
- 'x-apikey': "accesskey=d2ffdb13f78e4d008615d8be368d9739; 
-secretkey=b7748a7484fe4da6a6ced55e33b2b43c"
+ 'cookie': "TNS_SESSIONID=55668811",
+ 'x-apikey': "aadddddddd; 
+secretkey=aaaaaaa"
  }
 response = requests.request("GET", url, data=payload, headers=headers, 
 params=querystring)
